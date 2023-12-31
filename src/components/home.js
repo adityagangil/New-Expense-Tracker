2de0aefc35d4193d01bf742a8f42f397e6249149
[aditya gangil] <[adityagangil2001@gmail.com]>
2023-12-31 13:44:39 +0530

    second commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from './auth-context';
 
 const Home = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
     <div className="text-center">
       <h1>Welcome to Expense Tracker</h1>
+      {authCtx.isLoggedIn && !authCtx.isProfileComplete && (
+        <div>
+          <p>Your profile is incomplete. Please complete it.</p>
+          <Link to="/complete-profile">Complete Profile</Link>
+        </div>
+      )}
     </div>
   );
 };
